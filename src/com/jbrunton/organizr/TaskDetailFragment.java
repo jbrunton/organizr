5519ff1c926d222d89dcbb3b280c4338c283ffd8
jbrunton <john_brunton@hotmail.co.uk>
2013-04-02 23:04:27 +0100

    added text view to task details page

--- src/com/jbrunton/organizr/TaskDetailFragment.java
+++ src/com/jbrunton/organizr/TaskDetailFragment.java
@@ -54,7 +54,7 @@ public class TaskDetailFragment extends Fragment {
 
 		// Show the dummy content as text in a TextView.
 		if (mItem != null) {
-			((TextView) rootView.findViewById(R.id.task_detail))
+			((TextView) rootView.findViewById(R.id.description))
 					.setText(mItem.description);
 		}
 
