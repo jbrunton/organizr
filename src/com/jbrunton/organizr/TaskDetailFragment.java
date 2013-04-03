c960bf1a3971ff8ba83a4e1863de60b66052dba0
jbrunton <john_brunton@hotmail.co.uk>
2013-04-03 22:27:20 +0100

    implemented task details fragment

--- src/com/jbrunton/organizr/TaskDetailFragment.java
+++ src/com/jbrunton/organizr/TaskDetailFragment.java
@@ -1,10 +1,17 @@
 package com.jbrunton.organizr;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.jbrunton.organizr.models.Task;
@@ -24,7 +31,7 @@ public class TaskDetailFragment extends Fragment {
 	/**
 	 * The dummy content this fragment is presenting.
 	 */
-	private Task mItem;
+	private Task task;
 
 	/**
 	 * Mandatory empty constructor for the fragment manager to instantiate the
@@ -41,7 +48,7 @@ public class TaskDetailFragment extends Fragment {
 			// Load the dummy content specified by the fragment
 			// arguments. In a real-world scenario, use a Loader
 			// to load content from a content provider.
-			mItem = Task.TASK_MAP.get(getArguments().getString(
+			this.task = Task.TASK_MAP.get(getArguments().getString(
 					ARG_ITEM_ID));
 		}
 	}
@@ -53,11 +60,38 @@ public class TaskDetailFragment extends Fragment {
 				container, false);
 
 		// Show the dummy content as text in a TextView.
-		if (mItem != null) {
-			((TextView) rootView.findViewById(R.id.description))
-					.setText(mItem.description);
-		}
+		final EditText description = (EditText) rootView.findViewById(R.id.description);
+		final TextView completeLabel = (TextView) rootView.findViewById(R.id.complete_label);
+		final CheckBox complete = (CheckBox) rootView.findViewById(R.id.complete);
+		final int defaultColor = completeLabel.getTextColors().getDefaultColor();
 
+		complete.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				task.complete = buttonView.isChecked();
+				int paintFlags = completeLabel.getPaintFlags();
+				if (buttonView.isChecked()) {
+					paintFlags |= Paint.STRIKE_THRU_TEXT_FLAG;
+					completeLabel.setTextColor(Color.GRAY);
+				} else {
+					paintFlags ^= Paint.STRIKE_THRU_TEXT_FLAG;
+					completeLabel.setTextColor(defaultColor);
+				}
+				completeLabel.setPaintFlags(paintFlags);
+			}
+		});
+		
+		description.addTextChangedListener(new TextWatcher() {
+	        public void afterTextChanged(Editable s) {
+	        	task.description = description.getText().toString();
+	        }
+	        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+	        public void onTextChanged(CharSequence s, int start, int before, int count) {}
+	    });
+
+		description.setText(task.description);
+		complete.setChecked(task.complete);
+		
 		return rootView;
 	}
 }
